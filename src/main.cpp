a3c098cfbb19df815f21fdc2c47ef6d708835df2
Ivan Fisunov <Ivan.Fisunov@kaspersky.com>
2022-07-13 11:04:17 +0300

    Add hw02

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,31 @@
-#include "version.h"
-
 #include <iostream>
+#include <vector>
+#include <algorithm>
+
+#include "utils.h"
+
 
 int main(int, char **)
 {
-    std::cout << "Version: " << version() << std::endl;
-    std::cout << "Hello, world!" << std::endl;
+    IpPool ipPool{};
+
+    for (std::string line; std::getline(std::cin, line);) {
+        std::vector<std::string> ip = utils::split(line, '\t');
+        ipPool.push_back(utils::split(ip.at(0), '.'));
+    }
+
+    utils::sortIpPool(ipPool);
+    utils::print(ipPool);
+
+    IpPool filteredPool1 = utils::filter(ipPool, 1);
+    utils::print(filteredPool1);
+
+    IpPool filteredPool2 = utils::filter(ipPool, 46, 70);
+    utils::print(filteredPool2);
+
+    IpPool filteredPoolAny = utils::filter_any(ipPool, 46);
+    utils::print(filteredPoolAny);
+
+
     return 0;
 }
